349488637846f3651b83711511e93478d92a0010
Jcns <jcns@cin.ufpe.br>
2018-09-28 14:15:33 -0300

    DB Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_145108) do
+ActiveRecord::Schema.define(version: 2018_09_27_182448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cadastro_produtos", force: :cascade do |t|
+    t.string "categoria_produto"
+    t.string "produto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "grupo_caracteristicas", force: :cascade do |t|
+    t.text "caracteristicas"
+    t.bigint "cadastro_produto_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cadastro_produto_id"], name: "index_grupo_caracteristicas_on_cadastro_produto_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_09_26_145108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "grupo_caracteristicas", "cadastro_produtos"
 end
