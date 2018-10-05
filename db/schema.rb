0104428b1553e5750b91fe0215548ce467073d2c
Jcns <jcns@cin.ufpe.br>
2018-10-04 23:33:23 -0300

    removendo userapi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_005543) do
+ActiveRecord::Schema.define(version: 2018_09_30_035151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,6 @@ ActiveRecord::Schema.define(version: 2018_10_04_005543) do
     t.datetime "imagem_updated_at"
     t.index ["deleted_at"], name: "index_cad_produtos_on_deleted_at"
     t.index ["user_id"], name: "index_cad_produtos_on_user_id"
-  end
-
-  create_table "userapis", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
